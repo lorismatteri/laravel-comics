646b9d4a72081093823fd130ef535d6ffee3ee3e
Loris Matteri <loris.matteri93@gmail.com>
2021-03-03 16:12:40 +0100

    Struttura base base
    Sezioni admin e guests

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PageController@index')->name('homepage');
+Route::get('about', 'PageController@about')->name('about');
+Route::get('contacts', 'PageController@contacts')->name('contacts');
+
+Auth::routes();
+
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', 'HomeController@index')->name('index');
+    Route::resource('comics', 'ComicController');
+});
